afefc8fce98a6aa9bddaeab5572b1de3373007bf
sercan52863 <sercanburbano@gmail.com>
2025-05-22 14:08:22 -0400

    Create Header.js

---
+++ Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './styles/main.scss';
+
+const Header = () => {
+  return (
+    <header className="hero">
+      <div className="hero-content">
+        <h1>Velynt Luxury Rides</h1>
+        <p>Premium Araç Kiralama Deneyimi</p>
+      </div>
+    </header>
+  );
+};
+
+export default Header;
