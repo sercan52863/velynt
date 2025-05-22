40ba16abb1ef8c7597672219809fca784c7d1e81
sercan52863 <sercanburbano@gmail.com>
2025-05-22 13:33:30 -0400

    Create App.js

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReservationForm from './components/ReservationForm';
+
+function App() {
+  return (
+    <div className="app-container">
+      <header>
+        <h1>Velynt Luxury Rides</h1>
+      </header>
+      <ReservationForm />
+    </div>
+  );
+}
+
+export default App;
